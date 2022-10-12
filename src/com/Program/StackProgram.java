ad5c6325ef76a2bd0e20d914f6b75f73d5dd1f4c
ashutoshnawale89 <ashutoshnawale89@gmail.com>
2022-10-12 11:47:11 +0530

    Added: Stack Push Method

--- src/com/Program/StackProgram.java
+++ src/com/Program/StackProgram.java
@@ -1,8 +1,37 @@
 package com.Program;
 
 public class StackProgram {
+	// FOR NODE CLASS
+	class Node  {
+		int data;
+		Node next;
+		Node (int data){
+			this.data=data;
+			this.next=null;
+		}
+	}
+	// ADD - POSITION
+	static Node head;
+	public void Push(int data) {
+		Node newNode=new Node(data);
+		if (head == null) {
+			head=newNode; 
+			System.out.print(head.data+"->");
+		}
+		else {
+			newNode.next = head;
+			head = newNode;
+			System.out.print(head.data+"->");
+
+		}  
+	}
+
 	public static void main(String[] args) {
-		System.out.println("Welcome Stack Queue Program");
+		StackProgram obje=new StackProgram();
+		obje.Push(70);
+		obje.Push(30);
+		obje.Push(56);
+		System.out.print("null");
 	}
 }
 
